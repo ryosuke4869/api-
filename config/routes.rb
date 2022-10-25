e8c8aeb67c5524509b59310dac381860491647f1
{ryosuke4869} <{ryo-62-gnt-0000@outlook.jp}>
2022-10-25 18:54:45 +0900

    [Add]: #5 deviseの導入 deviseの導入に伴い、設定ファイル、User model、viewファイルの作成を行なった

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get '/',                        to: 'top#index'
   get 'index',                    to: 'top#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
