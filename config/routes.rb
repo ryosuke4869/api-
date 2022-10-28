96251eb9af149be0ad87da7698caa2ba17d92cc1
{ryosuke4869} <{ryo-62-gnt-0000@outlook.jp}>
2022-10-28 21:32:46 +0900

    [update]: #12  ゲストログイン機能の実装 ゲストログイン機能を実装するため修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  get '/',                        to: 'top#index'
-  get 'index',                    to: 'top#index'
+  get '/',                        to: 'tops#index'
+  get 'index',                    to: 'tops#index'
+
+  devise_scope :user do
+    post 'users/guest_sign_in',   to: 'users/sessions#guest_sign_in'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
