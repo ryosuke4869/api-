d43dac3041c3f883815cd73b6a1d232ce8fac45d
{ryosuke4869} <{ryo-62-gnt-0000@outlook.jp}>
2022-11-01 13:42:52 +0900

    [fix]: #15 新規登録、ログイン時に:nameの値がnullとなる現象を修正 新規登録、ログイン時に:nameの値を送ることを許可

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    # サインアップ時にusernameも追加で許可
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:user_name])
+    #プロフィール編集時にnameと自己紹介、アイコン画像のストロングパラメーターを渡す設定
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :introduce, :user_image])
+  end
 end
